f28b76ea9cb18bb0886e0b8fa2d44e1a108cd02b
kamel_fathy <58692124+kamel91@users.noreply.github.com>
2022-09-03 22:42:10 +0200

    food cart

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,22 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Hello there, I'm Kamel 
+         hope you like my project</h2>
+      <p>
+        I have been studying React for along time because I want to be a proficional developer 
+        in the near future
+        
+      </p>
+      <p>
+       I have experience in nome tecnologies like Next.js/ Css/ Scss/ Html/ Tsilwind/ Bootstrap/ 
+       in backend Node.js and Express.js and MongoDb  
+       
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
